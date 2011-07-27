8f086167bb5f92c92150b5255ccea6c6a55cc880
Adrian Schreyer <ams214@cam.ac.uk>
2011-07-27 09:44:26 +0100

    Changed Sphinx schema.

--- docs/conf.py
+++ docs/conf.py
@@ -91,19 +91,20 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'haiku'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+# textcolor
+html_theme_options = {'headingcolor':'#cc6600','linkcolor':'#dd7700','hoverlinkcolor' :'#dd7700', 'visitedlinkcolor':'#ddaa6e'}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = 'credoscript documentation'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
@@ -150,7 +151,7 @@ html_static_path = ['_static']
 #html_show_sourcelink = True
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
-#html_show_sphinx = True
+html_show_sphinx = False
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
 #html_show_copyright = True
