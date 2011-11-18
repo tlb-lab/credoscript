df2a03c8c82a9c74e866469aaca31ce00a5cef7e
Adrian Schreyer <ams214@cam.ac.uk>
2011-11-18 14:31:31 +0000

    The peptides table inherits all columns from residues now and ORM queries requiring either the biomolecule_id or chain_id have been modified.

--- adaptors/peptideadaptor.py
+++ adaptors/peptideadaptor.py
@@ -18,18 +18,11 @@ class PeptideAdaptor(object):
     def fetch_all_by_chain_id(self, chain_id, *expressions):
         '''
         '''
-        return self.query.join(
-            (Residue, Residue.residue_id==Peptide.residue_id)
-            ).filter(and_(Residue.chain_id==chain_id, *expressions)).all()
+        return self.query.filter(and_(Peptide.chain_id==chain_id, *expressions)).all()
 
     def fetch_all_by_biomolecule_id(self, biomolecule_id, *expressions):
         '''
         '''
-        return self.query.join(
-            (Residue, Residue.residue_id==Peptide.residue_id),
-            (Chain, Chain.chain_id==Residue.chain_id)
-            ).filter(and_(Chain.biomolecule_id==biomolecule_id, *expressions)).all()
+        return self.query.filter(and_(Peptide.biomolecule_id==biomolecule_id, *expressions)).all()
 
 from ..models.peptide import Peptide
-from ..models.residue import Residue
-from ..models.chain import Chain
