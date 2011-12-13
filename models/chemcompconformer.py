cbf2a33b7cf71c0a7a2ba9f96a633cd5164871e6
Adrian Schreyer <ams214@cam.ac.uk>
2011-12-13 17:09:59 +0000

    Added contained_in() expression that compiles into the PostgreSQL cube <@ operator. Also added usrcat() method.

--- models/chemcompconformer.py
+++ models/chemcompconformer.py
@@ -1,3 +1,5 @@
+from sqlalchemy.ext.hybrid import hybrid_method
+
 from credoscript import Base
 
 class ChemCompConformer(Base):
@@ -10,7 +12,22 @@ class ChemCompConformer(Base):
         '''
         return '<ChemCompConformer({self.het_id} {self.conformer})>'.format(self=self)
 
-    def usrcat(self):
+    @classmethod
+    def contained_in(self, cube):
+        '''
+        Returns a PostgreSQL cube database 'contained in' (<@) query as SQLAlchemy
+        binary expression.
+
+        Returns
+        -------
+        expression : sqlalchemy.sql.expression._BinaryExpression
+
+        '''
+        return self.usr_space.op('<@')(cube)
+        
+    def usrcat(self, *expressions, **kwargs):
         '''
         '''
-        pass
+        return ChemCompAdaptor().fetch_all_by_usr_moments(*expressions, usr_space=self.usr_space, usr_moments=self.usr_moments, **kwargs)
+
+from ..adaptors.chemcompadaptor import ChemCompAdaptor
