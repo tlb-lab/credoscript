d99cbf23fb736f3bd286dee138d71ebcf77b30a8
Adrian Schreyer <ams214@cam.ac.uk>
2012-01-30 14:26:34 +0000

    Each mapped entity now has a query property to query itself (like Django ORM). The new BaseQuery class also supports pagination of query results via paginate(). Added a workaround for the lack of OrderedDict in Python < 2.7.

--- mixins/base.py
+++ mixins/base.py
@@ -1,5 +1,13 @@
-from collections import OrderedDict
-from sqlalchemy.orm import class_mapper
+from math import ceil
+
+# workaround for Python < 2.7
+# the __meta__ attribute of all mapped classes will have the wrong order though!
+try: from collections import OrderedDict
+except ImportError: OrderedDict = dict
+
+from sqlalchemy.orm import class_mapper, Query
+
+from credoscript import Session
 
 class ClassProperty(property):
     '''
@@ -9,10 +17,102 @@ class ClassProperty(property):
     def __get__(self, cls, owner):
         return self.fget.__get__(None, owner)()
 
+class Pagination(object):
+    '''
+    Internal helper class returned by BaseQuery.paginate.  You
+    can also construct it from any other SQLAlchemy query object if you are
+    working with other libraries.  Additionally it is possible to pass None
+    as query object in which case the prev and next will no longer work.
+    
+    Adapted from the Flask-SQLAlchemy extension (http://packages.python.org/Flas
+    k-SQLAlchemy/api.html#flaskext.sqlalchemy.Pagination) 
+    '''
+    def __init__(self, query, page, per_page, total, items):
+        self.query = query
+        self.page = page
+        self.per_page = per_page
+        self.total = total
+        self.items = items
+
+    def __iter__(self):
+        '''
+        Returns an iterator over all the pages.
+        '''
+        for page in xrange(1, self.pages):
+            yield self.query.paginate(page, self.per_page)
+
+    @property
+    def pages(self):
+        '''
+        The total number of pages.
+        '''
+        return int(ceil(self.total / float(self.per_page)))
+
+    def prev(self):
+        '''
+        Returns Pagination object for the previous page.
+        '''
+        assert self.query is not None, 'a query object is required for this method to work'
+        return self.query.paginate(self.page - 1, self.per_page)
+
+    @property
+    def prev_num(self):
+        '''
+        Number of the previous page.
+        '''
+        return self.page - 1
+
+    @property
+    def has_prev(self):
+        '''
+        True if a previous page exists
+        '''
+        return self.page > 1
+
+    def next(self):
+        '''
+        Returns a Pagination object for the next page.
+        '''
+        assert self.query is not None, 'a query object is required for this method to work'
+        return self.query.paginate(self.page + 1, self.per_page)
+
+    @property
+    def has_next(self):
+        '''
+        True if a next page exists.
+        '''
+        return self.page < self.pages
+
+    @property
+    def next_num(self):
+        '''
+        Number of the next page
+        '''
+        return self.page + 1
+
+class BaseQuery(Query):
+    '''
+    Base query that will be attached to every model in the credoscript API.
+    
+    Adapted from the Flask-SQLAlchemy extension (http://packages.python.org/Flas
+    k-SQLAlchemy/api.html#flaskext.sqlalchemy.Pagination) 
+    '''
+    def paginate(self, page=1, per_page=10):
+        '''
+        '''
+        items = self.limit(per_page).offset((page - 1) * per_page).all()
+        
+        return Pagination(self, page, per_page, self.count(), items)
+
 class Base(object):
     '''
+    Declarative base model that is inherited by all CREDO models.
     '''
+    # automatically reflect the table
     __table_args__ = {'autoload':True}
+    
+    # attach a query object to every model that queries itself
+    query = Session.query_property(BaseQuery)
     
     @ClassProperty
     @classmethod
