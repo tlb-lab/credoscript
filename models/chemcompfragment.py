642fc541fb21763e9c031e05c91516d021ff026b
Adrian Schreyer <ams214@cam.ac.uk>
2012-03-20 16:41:25 +0000

    Sessions are now created locally.

--- models/chemcompfragment.py
+++ models/chemcompfragment.py
@@ -1,21 +1,23 @@
 from sqlalchemy.sql.expression import func
 
-from credoscript import Base, session, chem_comp_fragment_atoms
+from credoscript import Base, Session, chem_comp_fragment_atoms
 
 class ChemCompFragment(Base):
-    '''
-    '''
+    """
+    """
     __tablename__ = 'pdbchem.chem_comp_fragments'
     
     def __repr__(self):
-        '''
-        '''
+        """
+        """
         return '<ChemCompFragment({self.het_id} {self.fragment_id})>'.format(self=self)
 
     @property
     def pdb_atom_names(self):
-        '''
-        '''
+        """
+        """
+        session = Session()
+        
         query = session.query(chem_comp_fragment_atoms.c.hit, func.array_agg(chem_comp_fragment_atoms.c.pdb_name))
         query = query.filter(chem_comp_fragment_atoms.c.chem_comp_fragment_id==self.chem_comp_fragment_id)
         query = query.group_by(chem_comp_fragment_atoms.c.hit)
