9bb8188b4e53ae85187e3a4d0ca8ad798fc9eb92
Adrian Schreyer <ams214@cam.ac.uk>
2012-03-20 16:33:35 +0000

    Removed unecessary __init__ method. Added fetch_by_res_map_id() method.

--- adaptors/peptideadaptor.py
+++ adaptors/peptideadaptor.py
@@ -3,9 +3,11 @@ from sqlalchemy.sql.expression import and_
 from credoscript.mixins import PathAdaptorMixin, ResidueAdaptorMixin
 
 class PeptideAdaptor(ResidueAdaptorMixin, PathAdaptorMixin):
-    '''
-    '''
-    def __init__(self):
-        self.query = Peptide.query
+    """
+    """
+    def fetch_by_res_map_id(self, res_map_id):
+        """
+        """
+        return Peptide.query.filter_by(res_map_id=res_map_id).first()
        
 from ..models.peptide import Peptide
