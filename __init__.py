14c98d6eb5d0c8de842a5a2452e3bd7cbcf4df3d
Adrian Schreyer <ams214@cam.ac.uk>
2012-03-20 16:21:51 +0000

    Custom data types are now natively supported through psycopg2.new_type(). Only tables that are actually used in the API are reflected now, this should make import of credoscript faster. Removed session instance; session should be created ad hoc in the adaptors if necessary. Added shortcuts to two more tables.

--- __init__.py
+++ __init__.py
@@ -2,10 +2,14 @@ import os
 import json
 import warnings
 
+import psycopg2
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.exc import SAWarning
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+
+# register new data types
+import credoscript.util.psycopg2
 
 # the following modules will be imported with from credoscript import *
 __all__ = ['adaptors','contrib','extensions','models']
@@ -21,7 +25,6 @@ if not os.path.exists(CONFIG_PATH):
 config = json.loads(open(CONFIG_PATH).read())
 
 # database connection
-
 execution_options = dict(stream_results=config['connection']['stream_results'])
 
 url      = '{driver}://{user}:{passwd}@{host}:{port}/{db}'.format(**config['connection'])
@@ -33,13 +36,13 @@ metadata = MetaData(bind=engine)
 warnings.simplefilter('ignore', SAWarning)
 
 # reflect all the required schemas
-metadata.reflect(schema='credo')
-metadata.reflect(schema='pdbchem')
-metadata.reflect(schema='pdb')
+metadata.reflect(schema='credo', only=lambda t,m: t in config['schema']['credo']['reflect'])
+metadata.reflect(schema='pdbchem', only=lambda t,m: t in config['schema']['pdbchem']['reflect'])
+metadata.reflect(schema='pdb', only=lambda t,m: t in config['schema']['pdb']['reflect'])
+metadata.reflect(schema='variations', only=lambda t,m: t in config['schema']['variations']['reflect'])
 
 # database session
 Session = scoped_session(sessionmaker(bind=engine, autocommit=True))
-session = Session()
 
 # import Base here because the module imports the Session object
 from credoscript.mixins import Base
@@ -49,9 +52,11 @@ Base = declarative_base(bind=engine, metadata=metadata, cls=Base)
 
 # do not map against class
 binding_sites = metadata.tables['credo.binding_sites']
+binding_site_fuzcav = metadata.tables['credo.binding_site_fuzcav']
 interface_residues = metadata.tables['credo.interface_residues']
 binding_site_atom_surface_areas = metadata.tables['credo.binding_site_atom_surface_areas']
 chem_comp_fragment_atoms = metadata.tables['pdbchem.chem_comp_fragment_atoms']
 ligand_usr = metadata.tables['credo.ligand_usr']
+ligand_fcd = metadata.tables['credo.ligand_fcd']
 citations = metadata.tables['pdb.citations']
 disordered_regions = metadata.tables['pdb.disordered_regions']
