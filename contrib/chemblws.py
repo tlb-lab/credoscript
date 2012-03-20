aee31eef9ea48e8f1b2fb79fac6ccd77ea28b5cf
Adrian Schreyer <ams214@cam.ac.uk>
2012-03-20 16:27:57 +0000

    Updated ChEMBL web service contrib module.

--- contrib/chemblws.py
+++ contrib/chemblws.py
@@ -8,38 +8,24 @@ class ChEMBLWS(object):
     def __init__(self):
         '''
         '''
-        self._url = 'https://www.ebi.ac.uk/chemblws/{type}/{chembl_id}.json'
+        self._url = "https://www.ebi.ac.uk/chemblws/{entity}/{target}/{query}.json"
 
-    def _get_instance(self, type, chembl_id):
-        '''
-        '''
+    def _get_instance(self, entity, target, query):
+        """
+        """
+        url = self._url.format(entity=entity, target=target, query=query)
+        
         try:
-            response = urlopen(self._url.format(type=type, chembl_id=chembl_id))
-            return json.loads(response.read())
+            response = urlopen(url)   
 
         except HTTPError, error:
-            if error.code == 404: print 'Nothing found for ChEMBL identifier {chembl_id}.'.format(chembl_id=chembl_id)
-            else: print 'HTTP Error code: {code}'.format(code=error.code)
+            raise error 
+        
+        else:
+            return json.loads(response.read())
 
-        except URLError, error:
-            pass
-
-    def get_compound(self, chembl_id):
-        '''
-        '''
-        return self._get_instance('compounds', chembl_id)
-
-    def get_target(self, chembl_id):
-        '''
-        '''
-        return self._get_instance('targets', chembl_id)
-
-    def get_assay(self, chembl_id):
-        '''
-        '''
-        return self._get_instance('assays', chembl_id)
-
-    def get_targets(self):
-        '''
-        '''
-        pass
+    def compound_bioactivities(self, chembl_id):
+        """
+        Get individual compound bioactivities
+        """
+        return self._get_instance('compounds', chembl_id, 'bioactivities')
