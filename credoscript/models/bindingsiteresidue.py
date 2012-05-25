32bb15cb07a0967ea17997b68a33c2805bc29977
Adrian Schreyer <ams214@cam.ac.uk>
2012-05-25 12:38:37 +0100

    Updated relationship between BindingSiteResidue and Residue to include the entity_type_bm on the primary join as well. This avoids scanning of unnecessary partitions. Also added a docstring to the BindingSiteResidue class.

--- credoscript/models/bindingsiteresidue.py
+++ credoscript/models/bindingsiteresidue.py
@@ -2,11 +2,13 @@ from sqlalchemy.orm import backref, relationship
 from credoscript import Base
 
 class BindingSiteResidue(Base):
-    '''
-    '''
+    """
+    This class represents a row from the mapping between ligands and the residues
+    they interact with, including solvents and other ligands.
+    """
     __tablename__ = 'credo.binding_site_residues'
 
     Residue = relationship("Residue",
-                           primaryjoin = "Residue.residue_id==BindingSiteResidue.residue_id",
-                           foreign_keys = "[Residue.residue_id]",
+                           primaryjoin="and_(Residue.residue_id==BindingSiteResidue.residue_id, Residue.entity_type_bm==BindingSiteResidue.entity_type_bm)",
+                           foreign_keys="[Residue.residue_id, Residue.entity_type_bm]",
                            uselist=False, innerjoin=True)
